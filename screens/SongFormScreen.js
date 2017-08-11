28f55fd2146fb7eab78e583f8eb9c3459849b3a2
Saimon Moore <saimonmoore@gmail.com>
2017-08-11 09:00:04 +0200

    Fix styling

--- screens/SongFormScreen.js
+++ screens/SongFormScreen.js
@@ -31,19 +31,7 @@ class SongFormScreen extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+    justifyContent: 'flex-start',
   },
 });
 
